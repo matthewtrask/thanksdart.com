27ce39c2ef36ed5cc6f9837ae7c0c8a82a91d5dd
= <=>
2022-02-13 16:50:38 -0600

    add form

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -21,8 +21,115 @@
       <img class="w-full object-cover" src="../assets/dart/nationally-uncompetitive.jpg" alt="">
     </div>
     <div class="pt-10 pb-10">
-      <div class="w-2/3 mx-auto">
+      <div class="w-2/3 mx-auto pb-12">
         <h2 class="font-sans font-extrabold antialiased sm:text-1xl lg:text-3xl text-center drop-shadow-lg">The Dart Cycling Team was put together to find the fun in cycle racing. Started in Savannah, GA we have members across the United States. From Nashville, Tennessee to Washington, D.C. you can find someone in a Dart kit riding up front leading the group.</h2>
+      </div>
+      <div class="w-2/3 mx-auto">
+        <h2 class="pb-12 font-sans font-extrabold antialiased sm:text-1xl lg:text-3xl text-center drop-shadow-lg">Want to join us? Or have a question? Maybe just want to say hi?</h2>
+        <div class="rounded-md bg-green-50 p-4 max-w-md" v-if="showSuccess">
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <!-- Heroicon name: solid/check-circle -->
+              <svg class="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+              </svg>
+            </div>
+            <div class="ml-3">
+              <p class="text-sm font-medium text-green-800">Successfully uploaded</p>
+            </div>
+            <div class="ml-auto pl-3">
+              <div class="-mx-1.5 -my-1.5">
+                <button type="button" class="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600">
+                  <span class="sr-only">Dismiss</span>
+                  <!-- Heroicon name: solid/x -->
+                  <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="block p-6 rounded-lg shadow-lg bg-white max-w-md mx-auto">
+
+          <form action="https://fieldgoal.io/f/11Vje9" method="POST">
+            <div class="form-group mb-6">
+              <input type="text" class="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+                focus:text-gray-700 focus:bg-white focus:border-yellow-500 focus:outline-none
+" id="exampleInput7"
+                     placeholder="Name" name="name">
+            </div>
+            <div class="form-group mb-6">
+              <input type="email" class="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+                focus:text-gray-700 focus:bg-white focus:border-yellow-500 focus:outline-none
+" id="exampleInput8"
+                     placeholder="Email address" name="email">
+            </div>
+            <div class="form-group mb-6">
+      <textarea class="form-control block w-full px-3 py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-black focus:bg-white focus:border-yellow-500 focus:outline-none
+      "
+          id="exampleFormControlTextarea13"
+          rows="3"
+          placeholder="Message"
+                name="message"
+      ></textarea>
+            </div>
+            <button type="submit" class="
+      w-full
+      px-6
+      py-2.5
+      daddy-yellow
+      text-white
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      rounded
+      shadow-md
+      hover:bg-yellow-700 hover:shadow-lg
+      focus:bg-yellow-700 focus:shadow-lg focus:outline-none focus:ring-0
+      active:bg-yellow-800 active:shadow-lg
+      transition
+      duration-150
+      ease-in-out"
+            @click="sendForm()">Send</button>
+          </form>
+        </div>
+
       </div>
     </div>
     <div class="pt-12 pb-12 bg-black" id="sponsors">
@@ -44,15 +151,18 @@
     </div>
     <div class="pt-10 pb-10">
       <div class="w-2/3 mx-auto">
-        <div class="grid sm:grid-cols-1 sm:gap-1 lg:grid-cols-3 lg:gap-3">
+        <div class="grid grid-flow-col auto-cols-fr">
           <div v-for="team in founders">
             <img :src="getPic(team.img)" v-bind:alt="team.name + ' image'" class="mx-auto h-32 w-32 rounded-full">
             <h4 class="text-center tracking-wider text-3xl pb-4">{{ team.name }}</h4>
             <p class="text-center tracking-wider font-bold lg:h-12">{{ team.role }}</p>
-            <div class="socials h-12 sm:mb-12">
+            <div class="socials h-12 sm:mb-12 flex justify-center">
               <div v-if="Object.keys(team.socials).length > 0">
-                <a :href="team.socials.instagram" class="pb-12">
-                  <img class="sm:block h-10 w-auto justify-center mx-auto pb-4" src="../assets/instagram-brands.svg" alt="Workflow">
+                <a :href="team.socials.instagram" class="pb-12" v-if="team.socials.instagram">
+                  <img class="sm:block h-10 w-auto mx-auto pb-4" src="../assets/instagram-brands.svg" alt="Workflow">
+                </a>
+                <a :href="team.socials.strava" class="pb-12" v-if="team.socials.strava">
+                  <img class="sm:block h-10 w-auto mx-auto pb-4" src="../assets/strava-brands.svg" alt="Workflow">
                 </a>
               </div>
             </div>
@@ -63,15 +173,18 @@
     </div>
     <div class="pt-10 pb-10">
       <div class="w-2/3 mx-auto">
-        <div class="grid sm:grid-cols-2 sm:gap-2 sm:mb-12 lg:grid-cols-3 lg:gap-3">
+        <div class="grid sm:grid-cols-2 sm:gap-2 sm:mb-12 lg:grid-cols-3 lg:gap-3 auto-cols-max">
           <div v-for="team in teamMembers" class=" pb-12">
             <img :src="getPic(team.img)" v-bind:alt="team.name + ' image'" class="mx-auto h-32 w-32 rounded-full">
             <h4 class="text-center tracking-wider text-3xl pb-4">{{ team.name }}</h4>
             <p class="text-center tracking-wider font-bold lg:h-12">{{ team.role }}</p>
-            <div class="socials h-12 sm:mb-12">
+            <div class="socials h-12 sm:mb-12 flex justify-center">
               <div v-if="Object.keys(team.socials).length > 0">
-                <a :href="team.socials.instagram" class="pb-12">
-                  <img class="sm:block h-10 w-auto justify-center mx-auto pb-4" src="../assets/instagram-brands.svg" alt="Workflow">
+                <a :href="team.socials.instagram" class="pb-12 inline-block pr-4" v-if="team.socials.instagram">
+                  <img class="sm:block h-10 w-auto mx-auto pb-4" src="../assets/instagram-brands.svg" alt="Workflow">
+                </a>
+                <a :href="team.socials.strava" class="pb-12 inline-block pl-4"  v-if="team.socials.strava">
+                  <img class="sm:block h-10 w-auto mx-auto pb-4 text-orange-500	" src="../assets/strava-brands.svg" alt="Workflow">
                 </a>
               </div>
             </div>
@@ -98,19 +211,25 @@
     methods: {
       getPic(img) {
         return 'team/' + img;
+      },
+
+      sendForm() {
+        this.showSuccess = true;
       }
     },
     data() {
       return {
+        showSuccess: false,
         founders: [
           {
-            name: 'Gregg Cannon',
+            name: 'Gregg Gannon',
             role: 'Founder/Savannah Leader',
             bio: '',
             img: 'gregg.png',
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/unprotected_rider/',
+              strava: 'https://www.strava.com/athletes/1076514',
             }
           },
           {
@@ -121,6 +240,7 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/sidslaw/',
+              strava: 'https://www.strava.com/athletes/6468363',
             }
           },
           {
@@ -131,6 +251,7 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/ptp524/',
+              strava: null,
             }
           },
           {
@@ -141,6 +262,7 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/spackersss/',
+              strava: 'https://www.strava.com/athletes/996894',
             }
           },
           {
@@ -151,6 +273,7 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/jpmoncada/',
+              strava: 'https://www.strava.com/athletes/3110564',
             },
           },
         ],
@@ -161,7 +284,9 @@
             bio: '',
             img: 'brady.png',
             shavedLegs: true,
-            socials: {}
+            socials: {
+              strava: 'https://www.strava.com/athletes/15450549',
+            }
           },
           {
             name: 'Aubrey Moore',
@@ -171,6 +296,7 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/bigaubreezy/',
+              strava: 'https://www.strava.com/athletes/1407085',
             },
           },
           {
@@ -181,6 +307,7 @@
             img: 'hodges.png',
             socials: {
               instagram: 'https://www.instagram.com/hodgeshodges/',
+              strava: 'https://www.strava.com/athletes/1046112',
             }
           },
           {
@@ -190,7 +317,8 @@
             img: 'safety.png',
             shavedLegs: false,
             socials: {
-                instagram: 'https://www.instagram.com/___safetymichael/',
+              instagram: 'https://www.instagram.com/___safetymichael/',
+              strava: 'https://www.strava.com/athletes/2471926',
             }
           },
           {
@@ -200,7 +328,8 @@
             img: 'taylor.png',
             shavedLegs: false,
             socials: {
-                instagram: 'https://www.instagram.com/greatfortacos/',
+              instagram: 'https://www.instagram.com/greatfortacos/',
+              strava: 'https://www.strava.com/athletes/22762389'
             }
           },
           {
@@ -211,6 +340,7 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/conleyaaron/',
+              strava: 'https://www.strava.com/athletes/402999',
             }
           },
           {
@@ -221,6 +351,7 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/greenfleet/',
+              strava: 'https://www.strava.com/athletes/3187605',
             }
           },
           {
@@ -229,7 +360,8 @@
             img: 'sarah.png',
             shavedLegs: true,
             socials: {
-                instagram: 'https://www.instagram.com/coachsarahportella/',
+              instagram: 'https://www.instagram.com/coachsarahportella/',
+              strava: 'https://www.strava.com/athletes/4494379',
             }
           },
           {
@@ -239,7 +371,8 @@
             img: 'herchel.png',
             shavedLegs: false,
             socials: {
-                instagram: 'https://www.instagram.com/herchelp/',
+              instagram: 'https://www.instagram.com/herchelp/',
+              strava: 'https://www.strava.com/athletes/387523',
             }
           },
           {
@@ -250,6 +383,7 @@
             shavedLegs: false,
             socials: {
               instagram: 'https://www.instagram.com/wlcrump/',
+              strava: 'https://www.strava.com/athletes/8192',
             }
           },
 
@@ -261,6 +395,7 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/heathersoutherland/',
+              strava: 'https://www.strava.com/athletes/34434567',
             }
           },
 
@@ -272,6 +407,7 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/wrbauman/',
+              strava: 'https://www.strava.com/athletes/18045066',
             }
           },
           {
@@ -282,6 +418,7 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/thesemirealscrowell/',
+              strava: 'https://www.strava.com/athletes/7129270',
             }
           },
           {
@@ -292,6 +429,7 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/__mikejoe__/',
+              strava: 'https://www.strava.com/athletes/8192',
             }
           },
           {
@@ -302,6 +440,29 @@
             shavedLegs: true,
             socials: {
               instagram: 'https://www.instagram.com/shayhowe/',
+              strava: 'https://www.strava.com/athletes/8989985',
+            }
+          },
+          {
+            name: 'Michael Ringer',
+            role: 'Crit Racer',
+            bio: '',
+            img: 'michael.png',
+            shavedLegs: true,
+            socials: {
+              instagram: 'https://www.instagram.com/ringading16/',
+              strava: 'https://www.strava.com/athletes/6636930?oq=rin',
+            }
+          },
+          {
+            name: 'Chris Stephens',
+            role: 'Friend of the Team',
+            bio: '',
+            img: 'chris.png',
+            shavedLegs: true,
+            socials: {
+              instagram: 'https://www.instagram.com/stephensc4/',
+              strava: 'https://www.strava.com/athletes/9566038',
             }
           },
           {
@@ -321,5 +482,7 @@
 </script>
 
 <style scoped>
-
+  .daddy-yellow {
+    background-color: rgb(255,223,74)
+  }
 </style>
